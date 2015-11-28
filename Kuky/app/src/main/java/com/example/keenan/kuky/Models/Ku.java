094d3e291d0e4ec9595d62cc7422dfcd74b5837d
Patrick <patrickhuston1@gmail.com>
2015-11-28 13:05:38 -0500

    Adds model classes for Ku and KuResponse

---
+++ Kuky/app/src/main/java/com/example/keenan/kuky/Models/Ku.java
@@ -0,0 +1,46 @@
+package com.example.keenan.kuky.Models;
+
+public class Ku {
+
+    private Integer id;
+    private String content;
+    private Integer upvotes;
+    private Integer downvotes;
+    private Double lat;
+    private Double lon;
+    private String createdAt;
+    private String updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    public Integer getDownvotes() {
+        return downvotes;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+}
