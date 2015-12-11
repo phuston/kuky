87a9762170843694d4522488ab1633e60ba48a34
keenanzucker <keenan.zucker@students.olin.edu>
2015-12-11 15:32:25 -0500

    added view for content detail, need to inflate card and recycler views

--- Kuky/app/src/main/java/com/example/keenan/kuky/activities/DetailActivity.java
+++ Kuky/app/src/main/java/com/example/keenan/kuky/activities/DetailActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.keenan.kuky.R;
 import com.example.keenan.kuky.api.ApiClient;
@@ -16,6 +20,9 @@ import com.example.keenan.kuky.models.KuDetailResponse;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -27,12 +34,18 @@ public class DetailActivity extends AppCompatActivity {
     private Ku mKu;
     private ArrayList<Comment> mComments;
 
+    @Bind(R.id.detail_back_button) Button mBackButton;
+    @Bind(R.id.ku_card_detail_view) CardView mKuCard;
+    @Bind(R.id.comment_submit_button) Button mCommentSubmitButton;
+    @Bind(R.id.edit_comment_text) EditText mEditText;
+    @Bind(R.id.comment_feed_rv) RecyclerView mCommentRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Intent intent = getIntent();
         String ku_id = intent.getStringExtra(DetailActivity.KU_ID);
+//        ButterKnife.bind(this);
 
         fetchKuInfo(ku_id);
 
@@ -50,6 +63,22 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
     }
+
+    @OnClick(R.id.detail_back_button)
+    public void onDetailBackButtonClick(View view) {
+
+        //TODO: Go back to the FeedFragment
+
+    }
+
+    @OnClick(R.id.comment_submit_button)
+    public void onCommentSubmitButton(View view) {
+        Snackbar.make(view, "Your comment is submitted", Snackbar.LENGTH_LONG)
+                .setAction("Submit", null).show();
+    }
+
+
+
 
     public void fetchKuInfo(String ku_id){
         ApiClient.getKukyApiClient().getKuDetail(ku_id)
